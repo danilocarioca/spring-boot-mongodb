61fcbfe91b6a426d3e00d2d69b023b2529757b8d
Danilo <2021108315@uac.pt>
2023-03-01 13:19:37 -0100

    projeto criado

---
+++ src/main/java/pt/sata/springmongo/SpringmongoApplication.java
@@ -0,0 +1,13 @@
+package pt.sata.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongoApplication.class, args);
+	}
+
+}
