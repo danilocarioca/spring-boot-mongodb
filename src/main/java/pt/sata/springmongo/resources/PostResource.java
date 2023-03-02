015a471d9d441d7d86fd12e1e57c69f67317634f
Danilo <2021108315@uac.pt>
2023-03-02 11:51:43 -0100

    Obtendo post com id

---
+++ src/main/java/pt/sata/springmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package pt.sata.springmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pt.sata.springmongo.domain.Post;
+import pt.sata.springmongo.domain.User;
+import pt.sata.springmongo.dto.UserDTO;
+import pt.sata.springmongo.services.PostService;
+import pt.sata.springmongo.services.UserService;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/pt/sata/springmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package pt.sata.springmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.sata.springmongo.domain.Post;
+import pt.sata.springmongo.domain.User;
+import pt.sata.springmongo.dto.UserDTO;
+import pt.sata.springmongo.repository.PostRepository;
+import pt.sata.springmongo.repository.UserRepository;
+import pt.sata.springmongo.services.exception.ObjectNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
