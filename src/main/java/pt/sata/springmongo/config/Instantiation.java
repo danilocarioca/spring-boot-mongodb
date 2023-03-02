f64c5fca5b1346cb4e5996d566efb4762689c258
Danilo <2021108315@uac.pt>
2023-03-02 11:12:10 -0100

    Projeção dos dados do autor com DTO

--- src/main/java/pt/sata/springmongo/config/Instantiation.java
+++ src/main/java/pt/sata/springmongo/config/Instantiation.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import pt.sata.springmongo.domain.Post;
 import pt.sata.springmongo.domain.User;
+import pt.sata.springmongo.dto.AuthorDTO;
 import pt.sata.springmongo.repository.PostRepository;
 import pt.sata.springmongo.repository.UserRepository;
 
@@ -33,11 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/01/2023"), "Partiu viagem!","Vou viajar para os Açores, Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/01/2023"), "Bom dia","Acordei bem disposto hoje!", maria);
-
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/01/2023"), "Partiu viagem!","Vou viajar para os Açores, Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/01/2023"), "Bom dia","Acordei bem disposto hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
