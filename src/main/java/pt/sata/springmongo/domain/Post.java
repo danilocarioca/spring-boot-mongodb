aa97fb2b21cb0c80d3360b0547f4937210032b14
Danilo <2021108315@uac.pt>
2023-03-02 12:46:54 -0100

    Revert "Acrescentando comentários aos posts"
    This reverts commit f28b8a9c673e53f6bdb570e961d195f42ed94c80.

--- src/main/java/pt/sata/springmongo/domain/Post.java
+++ src/main/java/pt/sata/springmongo/domain/Post.java
@@ -3,12 +3,9 @@ package pt.sata.springmongo.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import pt.sata.springmongo.dto.AuthorDTO;
-import pt.sata.springmongo.dto.CommentDTO;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -20,8 +17,6 @@ public class Post implements Serializable {
     private String title;
     public String body;
     private AuthorDTO author;
-
-    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -72,14 +67,6 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
-    }
-
-    public List<CommentDTO> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<CommentDTO> comments) {
-        this.comments = comments;
     }
 
     @Override
